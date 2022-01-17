c4e4ee6145c2af16b35e23fea401acf208744572
Sanjna Panwar <sanjnapanwar7697@gmail.com>
2022-01-17 16:05:58 +0530

    JS_Advance

---
+++ pureFunction.js
@@ -0,0 +1,31 @@
+// // pure function
+// var x=10;
+// function add(x,y){
+    
+//     return (x+y);
+// }
+// console.log(add(3,2));
+// console.log(add(6,2));
+// console.log(add(7,2));
+// console.log(add(7,2));
+
+
+// // impure function
+// 1st
+var y=5;
+const c=(x)=>{
+    y++
+    console.log(x+y)
+}
+c(2)
+
+// //2nd
+// var x=10;
+// function add(a,b){
+//     console.log(x);
+//    x++;
+// }
+// add()
+// add()
+// add()
+// add()
