a42c8c31e3e5b209746594a52a4d0fbbcd8ceae1
Sanjna Panwar <sanjnapanwar7697@gmail.com>
2022-01-24 14:25:27 +0530

    thisKeyword

--- this.js
+++ this.js
@@ -11,3 +11,19 @@
 // let name="Sanjna";
 // let lastName="Panwar";
 // console.log(name+" "+this.lastName)
+
+
+
+// // this keyword in JavaScript
+// //1st
+// let x=this;
+// console.log(typeof x);
+
+// //2nd
+// function myfun(){
+//     this.x=100
+// }
+// var obj=new myfun();
+// obj.k=12;
+// obj.l=90;
+// console.log(obj)
